e2e93b115f6ddb770fcf86763780eb7d806d4ca8
Alex Drozd <drozdster@gmail.com>
2021-10-02 12:55:25 -0500

    fixing django settings

--- kicksaw_integration_utils/settings.py
+++ kicksaw_integration_utils/settings.py
@@ -1,10 +1,23 @@
-from tempfile import gettempdir
+import re
+import sys
 
+from tempfile import gettempdir
 
 TEMP = gettempdir()
 
 try:
-    from django.conf.settings import *
+    # settings is not a module; it's an instance of a class
+    from django.conf import settings
+
+    current_module = sys.modules[__name__]
+
+    # loop through class properties
+    for setting in dir(settings):
+        # if the property name matches the variable name for a django setting,
+        # e.g., INSTALLED_APPS, set it as a variable for this module
+        # with the same name
+        if re.match("^[A-Z]+(?:_[A-Z]+)*$", setting):
+            setattr(current_module, setting, getattr(settings, setting))
 except ModuleNotFoundError:
     pass
 
